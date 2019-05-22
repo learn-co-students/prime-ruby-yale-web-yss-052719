44ccb51f3532cfeca98ca9465659e70e6c39a515
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 15:36:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int == 2
+    return true
+  else
+    num_array = [*2...int]
+    num_array.each do |element|
+      # if the number is divisible by one lower than it add 1 to counter
+      if int % element == 0
+        return false
+        break
+      else
+      end
+    end
+    return true
+  end
+end
